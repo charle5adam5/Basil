d528eaf4505d7da19005ad7902e76239db75888d
Charles <adams.charles00@gmail.com>
2018-07-11 13:17:27 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,65 @@
+// Gallery selectors
+const gallery = document.querySelector('.gallery-box');
+const close = document.querySelector('.close');
+const sea = document.querySelector('.sea');
+const land = document.querySelector('.land');
+const other = document.querySelector('.other');
+
+// info selectors
+const info = document.querySelectorAll('.info');
+const galItem = document.querySelectorAll('.gal-item');
+const galSec = document.querySelectorAll('.gal-sec');
+const partial = document.querySelectorAll('.partial');
+
+// gallery image selector
+const gridItem = document.querySelectorAll('.grid-itm');
+
+// toggling view mode for info
+info.forEach(item => {
+    item.addEventListener('click', () => {
+        partial.forEach(img => {
+            img.classList.toggle('lrg-partial');
+        })
+        if (item.className != 'view-info') {
+            item.classList.toggle('view-info');
+            viewBtn.classList.add('hide-btn');
+        } 
+    })
+})
+
+
+// open items
+galItem.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        this.index = index;
+        close.classList.add('show');
+        close.classList.add('styledClose');
+        gallery.classList.add('galSlide');
+        gallery.classList.remove('slideBack');
+        // open sections
+            galSec.forEach((section, index) => {
+                if(this.index == index) {
+                    section.classList.add('show');
+                }
+            })
+        });
+    });
+
+// close sections
+close.addEventListener('click', () => {
+ gallery.scrollIntoView({ behavior: 'smooth' });
+ close.classList.remove('show');
+ close.classList.remove('styledClose');
+ gallery.classList.remove('galSlide');
+ gallery.classList.add('slideBack');
+ sea.classList.remove('show');
+ land.classList.remove('show');
+ other.classList.remove('show');
+})
+
+// expand images
+gridItem.forEach(item => {
+    item.addEventListener('click', () => {
+        item.classList.toggle('lrg-img');
+    })
+})
