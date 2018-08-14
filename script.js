bb399b6fcb72c7676d80bbb4596b1c1e85481645
Charles <adams.charles00@gmail.com>
2018-08-14 16:33:04 -0400

    edits

--- script.js
+++ script.js
@@ -40,28 +40,6 @@ for (let i = 0; i < info.length; i ++) {
             })       
 }
 
-// info.forEach(item => {
-//     item.addEventListener('click', () => {
-//         item.scrollTo({ top: 0 });
-//         partial.forEach(img => {
-//             if(img.className === 'lrg-partial') {
-//                 img.className = 'partial';
-//             } else {
-//                 img.className = 'lrg-partial';
-//             }
-//             // img.classList.toggle('lrg-partial');
-       
-//         })
-//             if(item.className === 'view-info') {
-//                 item.className = 'info';
-//             } else {
-//                 item.className = 'view-info';
-//             }
-//             // item.classList.toggle('view-info');
-            
-//     })
-// })
-
 
 
 // open items
@@ -81,22 +59,6 @@ for (let i = 0; i < galItem.length; i++) {
     })
 }
 
-// old ie hates .forEach
-// galItem.forEach((item, index) => {
-//     item.addEventListener('click', () => {
-//         this.index = index;
-//         close.className = 'styledClose';
-//         gallery.className = gallery.className.replace(/slideBack/gi, '');
-//         gallery.className = 'galSlide';
-//         contact.className = 'contact-moved';
-//         // open sections
-//             galSec.forEach((section, index) => {
-//                     if(this.index == index) {
-//                         section.className = 'show';
-//                 }
-//         })
-//     })
-// })
 
 // close sections
 close.addEventListener('click', () => {
@@ -105,9 +67,9 @@ close.addEventListener('click', () => {
  close.className = 'close';
  gallery.className = gallery.className.replace(/galSlide/gi, 'slideBack');
  contact.className = contact.className.replace(/contact-moved/gi, '');
- galSec.forEach(section => {
-    section.className = section.className.replace(/show/gi, 'gal-sec');
-    })
+ for(let i = 0; i < galSec.length; i++) {
+     galSec[i].className = galSec[i].className.replace(/show/gi, 'gal-sec');
+ }
 })
 
 // expand images
@@ -121,12 +83,3 @@ for(let i = 0; i < gridItem.length; i++) {
     })
 }
 
-// gridItem.forEach(item => {
-//     item.addEventListener('click', () => {
-//         if(item.className != 'lrg-img') {
-//             item.className = 'lrg-img';
-//         } else {
-//             item.className = 'grid-itm';
-//         }
-//     })
-// })
